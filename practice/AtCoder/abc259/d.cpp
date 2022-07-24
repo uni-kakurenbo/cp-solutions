64ed3dafb383d5cbf8940e4456562fa6e11de8f2
uni_kakurenbo <kohma1116@ybb.ne.jp>
2022-07-24 15:41:21 +0900

    ABC 259 - D

--- practice/AtCoder/abc259/d.cpp
+++ practice/AtCoder/abc259/d.cpp
@@ -1,25 +1,32 @@
+/*
+ * @uni_kakurenbo
+ * https://github.com/uni-kakurenbo/competitive-programming-workspace
+ *
+ * CC0 1.0  http://creativecommons.org/publicdomain/zero/1.0/deed.ja
+ */
+
+/* #region template */
 #include <bits/stdc++.h>
 using namespace std;
-
-#include <atcoder/dsu>
-using namespace atcoder;
 
 #ifdef LOCAL_JUDGE
     #include<debug>
     #define debug(...) Debug::debug(Debug::split(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
-    #define DEBUG(...) do { Debug::DEBUG(nullptr, "#" + to_string(__LINE__) + "  "); Debug::DEBUG(__VA_ARGS__); Debug::DEBUG(nullptr, "\033[m"); } while(0);
+    #define DEBUG(...) do { Debug::DEBUG(nullptr, "\033[3;35m#" + to_string(__LINE__) + "\033[m  "); Debug::DEBUG(__VA_ARGS__); Debug::DEBUG(nullptr, "\033[m\n"); } while(0);
 #else
     #define debug(...) { ; }
     #define DEBUG(...) { ; }
 #endif
 
-using ll = long long;
-using ull = unsigned long long;
+#define INF32 2147483647
+#define INF64 9223372036854775807LL
 
 #define until(...) while(!(__VA_ARGS__))
 
 #define REP(i,n) for(int i=0, i##_length=int(n); i<i##_length; ++i)
+#define REPD(i,n) for(int i=n-1; i>=0; --i)
 #define FOR(i,a,b) for(ll i=a, i##_last=ll(b); i<=i##_last; ++i)
+#define FORD(i,a,b) for(ll i=a, i##_last=ll(b); i>=i##_last; --i)
 #define FORA(i,I) for(auto& i:I)
 
 #define ALL(x) begin(x),end(x)
@@ -28,8 +35,17 @@ using ull = unsigned long long;
 #define F$ first
 #define S$ second
 
+using ll = long long;
+using ull = unsigned long long;
+
+template<class T> inline T mod(T &x, T &r) { return (x%r+r)%r; }
+
 template<class T> inline bool chmax(T &a, T b) { return (a<b ? a=b, true : false); }
 template<class T> inline bool chmin(T &a, T b) { return (a>b ? a=b, true : false); }
+/* #endregion */
+
+#include <atcoder/dsu>
+using namespace atcoder;
 
 struct Circle {
     int x, y, r;
