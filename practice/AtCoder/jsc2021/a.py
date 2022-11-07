ee81674724d77cdd2940edd4cd2d6e0bdb6f7ea2
uni_kakurenbo <kohma1116@ybb.ne.jp>
2022-11-07 18:44:29 +0900

    2022/11/07 18:44

---
+++ practice/AtCoder/jsc2021/a.py
@@ -0,0 +1,14 @@
+# @uni_kakurenbo
+# https://github.com/uni-kakurenbo/competitive-programming-workspace
+#
+# CC0 1.0  http://creativecommons.org/publicdomain/zero/1.0/deed.ja
+
+# #language PyPy3 #
+
+from sys import setrecursionlimit, stderr, argv
+# setrecursionlimit(10**5)
+def debug(*args, **opts):
+    if argv[-1] == "LOCAL_JUDGE": print(*args, **opts, file=stderr)
+
+x, y, z = map(int, input().split())
+print((y * z - 1) // x)
---
+++ practice/AtCoder/panasonic2020/a.py
@@ -0,0 +1,15 @@
+# @uni_kakurenbo
+# https://github.com/uni-kakurenbo/competitive-programming-workspace
+#
+# CC0 1.0  http://creativecommons.org/publicdomain/zero/1.0/deed.ja
+
+# #language PyPy3 #
+
+from sys import setrecursionlimit, stderr, argv
+# setrecursionlimit(10**5)
+def debug(*args, **opts):
+    if argv[-1] == "LOCAL_JUDGE": print(*args, **opts, file=stderr)
+
+A = (1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51)
+k = int(input())
+print(A[k-1])
