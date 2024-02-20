4a5000d71ff4185fbb0a98171237f2c3f4105539
uni_kakurenbo <kohma1116@ybb.ne.jp>
2024-02-20 18:25:43 +0900

    2024/02/20 18:25

---
+++ practice/AtCoder/abc340/f.cpp
@@ -0,0 +1,29 @@
+/*
+ * @uni_kakurenbo
+ * https://github.com/uni-kakurenbo/competitive-programming-workspace
+ *
+ * CC0 1.0  http://creativecommons.org/publicdomain/zero/1.0/deed.ja
+ */
+/* #language C++ 20 GCC */
+#include "template/standard.hpp"
+
+void solve();
+
+signed main() {
+    debug(__cplusplus);
+    int $ = 1;
+    // std::cin >> $;
+    for(int _ = 0; _ < $; ++_) {
+        DEBUG("Case: #" + std::to_string(_));
+        solve();
+    }
+    return 0;
+}
+
+void solve() {
+    i64 x, y; input >> x >> y;
+
+    const auto ans = lib::bezout_equation<i64>(y, -x, 2);
+    if(ans) print(ans.value());
+    else print(-1);
+}
